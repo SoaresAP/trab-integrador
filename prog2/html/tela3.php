16c1fef8f343aa6838350cc545fa011ced357aa3
natani3l <nataniel.lemes@estudante.uffs.edu.br>
2019-11-20 17:29:48 -0300

    includes do head adicionados

--- prog2/html/tela3.php
+++ prog2/html/tela3.php
@@ -17,55 +17,9 @@
     <body>
 
         <div class="grid-conteiner" id="tela">
-            <header class="grid-conteiner">
-              <div id="categorias">
-                  <img src="../img/menu.png" alt="opcoes">
-
-              </div>
-                <div id="logo">
-                    <img src="../img/vitari.jpg">
-                </div>
-                <div class="barra" tabindex="0">
-                    <nav>
-                        <a href="#"><div class="link">UNHAS</div></a>
-                        <a href="#"><div class="link">MAQUIAGEM</div></a>
-                        <a href="#"><div class="link">CABELOS</div></a>
-                        <a href="#"><div class="link">KITS</div></a>
-                        <a href="#"><div class="link">HIGIENE</div></a>
-                        <a href="#"><div class="link">PELE</div></a>
-                        <a href="#"><div class="link">PERFUME</div></a>
-
-                    </nav>
-
-                </div>
-
-                <div id="cadastro">
-                    <a href="#"><img src="../img/cadastro.png"></a>
-
-                </div>
-                <div id="login">
-
-                    <a href="#"><img src="../img/login.png"></a>
-
-                </div>
-                <div id="pesquisa">
-                    <div id="divBusca">
-                        <input type="text" id="txtBusca" placeholder="Pesquisar..."/>
-                        <img src="../img/lupa.png" id="btnBusca" width="16" height="16" alt="Buscar"/>
-                    </div>
-                </div>
-                <div id="barra-categorias">
-                    <nav>
-                      <a href="#">UNHAS</a> |
-                      <a href="#">MAQUIAGEM</a> |
-                      <a href="#">CABELOS</a> |
-                      <a href="#">KITS</a> |
-                      <a href="#">HIGIENE</a> |
-                      <a href="#">PELE</a> |
-                      <a href="#">PERFUME</a>
-                    </nav>
-                </div>
-            </header>
+            
+            <!--HEAD-->
+            <?php include "../includes/head1.php"; ?>
 
             <main class="grid-conteiner">
                 <div class="imagem">IMAGEM</div>

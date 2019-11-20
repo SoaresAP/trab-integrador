b3fa5d991a26d1cd03884fa0d0499e5850850506
SoaresAP <soaresleonardo06@gmail.com>
2019-11-20 20:38:47 -0300

    adicionado head3.php

--- prog2/html/tela14.php
+++ prog2/html/tela14.php
@@ -22,46 +22,8 @@
 
         <div class="grid-conteiner" id="tela">
 
-            <header class="grid-conteiner">
-                <div class="barra">
-                    <nav>
-                        <a href="tela14.php">
-                            <div class="link">USUÁRIOS</div>
-                        </a>
-                        <a href="tela10.php">
-                            <div class="link">PRODUTOS</div>
-                        </a>
-                    </nav>
-                </div>
-
-                <div id="opcoes">
-                    <img src="../img/menu.png" alt="opcoes">
-                </div>
-                <div id="usuarios">
-                    <a href="tela14.php">USUÁRIOS</a>
-                </div>
-                <div id="produtos">
-                    <a href="tela10.php">PRODUTOS</a>
-                </div>
-
-                <div id="logo">
-                    <a href="tela1.php">
-                        <img src="../img/vitari.jpg" alt="logo">
-                    </a>
-                </div>
-                <div id="perfil">
-                    <a href="#">
-                        <img src="../img/perfil.png" alt="perfil">
-                    </a>
-                </div>
-                <div id="sair">
-                    <a href="#">SAIR</a>
-                </div>
-
-                <div id="pesquisa">
-                    <input type="text" placeholder="Pesquisar produtos..."/>
-                </div>
-            </header>
+          <!--HEAD-->
+          <?php include "../includes/head3.php"; ?>
 
             <main class="grid-conteiner">
                 <div id="tabela">

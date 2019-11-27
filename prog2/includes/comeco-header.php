9e3f0eb786ff51ea1a5aaea0783ea90b350712f0
PauloGSC <paulogscomasetto@gmail.com>
2019-11-27 10:43:19 -0300

    correcoes nos includes do header

---
+++ prog2/includes/comeco-header.php
@@ -0,0 +1,35 @@
+<header class="grid-conteiner">
+    <div class="barra">
+        <nav>
+            <a href="gerenciamento-u-la.php">
+                <div class="link">USUÁRIOS</div>
+            </a>
+            <a href="pesquisa-la.php">
+                <div class="link">PRODUTOS</div>
+            </a>
+        </nav>
+    </div>
+
+    <div id="opcoes">
+        <img src="../img/menu.png" alt="opcoes">
+    </div>
+    <div id="usuarios">
+        <a href="gerenciamento-u-la.php">USUÁRIOS</a>
+    </div>
+    <div id="produtos">
+        <a href="pesquisa-la.php">PRODUTOS</a>
+    </div>
+
+    <div id="logo">
+        <a href="principal-sl.php">
+            <img src="../img/vitari.jpg" alt="logo">
+        </a>
+    </div>
+    <div id="perfil">
+        <a href="#">
+            <img src="../img/perfil.png" alt="perfil">
+        </a>
+    </div>
+    <div id="sair">
+        <a href="#">SAIR</a>
+    </div>

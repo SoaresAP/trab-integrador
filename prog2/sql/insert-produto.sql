426fb42e9a3ce1c08877636ecc0424afc9c6b1a1
paulogsc <paulogscomasetto@gmail.com>
2019-12-02 16:18:29 -0300

    correcoes nos insert de produtos

--- prog2/sql/insert-produto.sql
+++ prog2/sql/insert-produto.sql
@@ -1,18 +1,15 @@
 INSERT INTO produto
 (cod, nome, ctipo, ccat, valor, dtfab, dtval, descr, img)
 VALUES
-(11111, 'Shampoo Palmolive', 1, 3, 30.50, '2000/04/21', '2000/10/25', 'Shampoo Palmolive ervas 400ml', 'Shampoo-Palmolive-Naturals-Nutri-Liss-Sem-Sal-350ml-2188.12.jpg'),
-(87677, 'Shampoo Palmolive', 1, 3, 20.5, '2000/04/21', '2000/10/25', 'Shampoo Palmolive liso perfeito 400ml', 'Shampoo-Palmolive-Naturals-Oleo-Argan-350ml-1289.63.jpg'),
-(22222, 'Perfume Malbec', 2, 7, 40.0, '2010/06/06', '2010/08/08', 'Perfume Malbec amadeirado nuances de eucalipto', 'malbec.jpg'),
-(33333, 'Esmalte', 4, 1, 22.0, '2005/02/02', '2005/09/09', 'Esmalte impala branco neve', 'Esmalte-Impala-Cremoso-Branco-10702.24.jpg'),
-(44444, 'Perfume Styletto', 2, 7, 50.0, '2007/08/15', '2009/09/21', 'Perfume Styletto suave 100ml', NULL),
-(55555, 'Mascara tratamento', 5, 3, 50.0, '2007/08/15', '2009/09/21', 'Mascara tratamento Salon cachos 400ml', 'Mascara-Salon-Line-Cachos-dos-Sonhos--ToDeCacho-500g-21762.04.jpg'),
-(66666, 'Demaquilante', 8, 6, 50.0, '2007/08/15', '2009/09/21', 'Demaquilante', NULL),
-(77777, 'Desodorante', 9, 5, 50.0, '2007/08/15', '2009/09/21', 'Desodorante Nivea men care 100ml', NULL),
-(88888, 'Algodão Toptop', 10, 2, 50.0, '2007/08/15', '2009/09/21', 'Algodão TopTop 100g', NULL),
-(12345, 'Cotonete cotton', 10, 5, 50.0, '2007/08/15', '2009/09/21', 'Hastes flexíves cotton 100Un', NULL),
-(56786, 'Condicionador Inoar', 11, 3, 50.0, '2007/08/15', '2009/09/21', 'Condicionador Inoar fragrâncias 300ml', NULL),
-(94588, 'Sabonete Liquido', 12, 5, 50.0, '2007/08/15', '2009/09/21', 'Sabonete liquido Protex 250ml', NULL);
-
-
->>>>>>> 75cb7b8d66c3eb00d1e7ad9a0e86ab57847d82ab
+(11111, 'Shampoo Palmolive', 1, 3, 30.50, '2000/04/21', '2000/10/25', 'Shampoo Palmolive ervas 400ml', '../img/default.png'),
+(87677, 'Shampoo Palmolive', 1, 3, 20.5, '2000/04/21', '2000/10/25', 'Shampoo Palmolive liso perfeito 400ml', '../img/default.png'),
+(22222, 'Perfume Malbec', 2, 7, 40.0, '2010/06/06', '2010/08/08', 'Perfume Malbec amadeirado nuances de eucalipto', '../img/default.png'),
+(33333, 'Esmalte', 4, 1, 22.0, '2005/02/02', '2005/09/09', 'Esmalte impala branco neve', '../img/default.png'),
+(44444, 'Perfume Styletto', 2, 7, 50.0, '2007/08/15', '2009/09/21', 'Perfume Styletto suave 100ml', '../img/default.png'),
+(55555, 'Mascara tratamento', 5, 3, 50.0, '2007/08/15', '2009/09/21', 'Mascara tratamento Salon cachos 400ml', '../img/default.png'),
+(66666, 'Demaquilante', 8, 6, 50.0, '2007/08/15', '2009/09/21', 'Demaquilante', '../img/default.png'),
+(77777, 'Desodorante', 9, 5, 50.0, '2007/08/15', '2009/09/21', 'Desodorante Nivea men care 100ml', '../img/default.png'),
+(88888, 'Algodão Toptop', 10, 2, 50.0, '2007/08/15', '2009/09/21', 'Algodão TopTop 100g', '../img/default.png'),
+(12345, 'Cotonete cotton', 10, 5, 50.0, '2007/08/15', '2009/09/21', 'Hastes flexíves cotton 100Un', '../img/default.png'),
+(56786, 'Condicionador Inoar', 11, 3, 50.0, '2007/08/15', '2009/09/21', 'Condicionador Inoar fragrâncias 300ml', '../img/default.png'),
+(94588, 'Sabonete Liquido', 12, 5, 50.0, '2007/08/15', '2009/09/21', 'Sabonete liquido Protex 250ml', '../img/default.png');
